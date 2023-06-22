5fe1b5f0471c91042d84fac64a032d1d0ade8ef8
Labici Ivan <labici.ioan@yaoo.com>
2023-06-22 22:50:03 +0300

    DG-1: Home Page Development #1
    - started the development on the home page.
    
    - continuing to build the carousel image section.

--- src/views/guest/home/Home.jsx
+++ src/views/guest/home/Home.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './home.css';
 
+import Dogo_picture from '../../../assets/dogo_picture_one.jpg'
+
 const Home = () => {
 
     const appName = import.meta.env.VITE_APP_NAME;
 
-    return ( 
-        <div className="home">Welcome to {appName}</div>
+    return (
+        <React.Fragment>
+            <div className="home">
+                <div className="home-carousel_section">
+                    <img className='carousel_section' src={Dogo_picture} alt="" />
+                </div>
+
+                <div className="home-ending_section">
+                    <div className="first_section-text"></div>
+                </div>
+            </div>
+
+
+            <div className="home-secondary_section">Welcome to {appName}</div>
+        </React.Fragment>
      );
 }
  
