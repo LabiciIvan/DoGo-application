5625031d8069c0756c34851988c72f38a1dcd91d
Labici Ivan <labici.ioan@yaoo.com>
2023-06-24 23:51:42 +0300

    DG-1: Home Page Development #1
    - added some basic text and styled it.

--- src/views/guest/home/Home.jsx
+++ src/views/guest/home/Home.jsx
@@ -34,7 +34,13 @@ const Home = () => {
                 </div>
 
                 <div className="home-ending_section">
-                    <div className="first_section-text"></div>
+                    <div className="filler"></div>
+                    <div className="first_section-text">
+                        <h4 className='first_section-header'>Your trusted partner when it comes to Dogs.</h4>
+                        <h4 className='_content-one'>Unleash boundless possibilities with DoGo, the leading provider of dog welfare solutions!</h4>
+                        <h4 className='_content-two'>Established in the world of dog care in 2023, we've built a reputation for providing services that go above and beyond.</h4>
+                        <h4 className='_content-three'>magine a partner who not only seamlessly integrates with your dog-related initiatives and values but consistently delivers life-changing solutions that elevate the well-being of our furry friends.</h4>
+                    </div>
                 </div>
             </div>
 
