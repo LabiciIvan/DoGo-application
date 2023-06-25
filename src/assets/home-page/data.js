ea26b092b78446921f7e7d002c8c7e6821fd8d14
Labici Ivan <labici.ioan@yaoo.com>
2023-06-25 14:06:39 +0300

    DG-1: Home Page Development #1
    - separated text content from within home view.
    
    - importing text and all content from /assets/ directory and finding the
      coresponding directory for each page view.

---
+++ src/assets/home-page/data.js
@@ -0,0 +1,52 @@
+const firstViewDescription = [
+	{
+		id: 1,
+		class: 'first_section-header',
+		content: 'Your trusted partner when it comes to Dogs.'
+	},
+	{	id: 2,
+		class: '_content-one',
+		content: 'Unleash boundless possibilities with DoGo, the leading provider of dog welfare solutions!'
+	},
+	{	id: 3,
+		class: '_content-two',
+		content: 'Established in the world of dog care in 2023, we\'ve built a reputation for providing services that go above and beyond.'
+	},
+	{
+		id: 4,
+		class: '_content-three',
+		content: 'Imagine a partner who not only seamlessly integrates with your dog-related initiatives and values but consistently delivers life-changing solutions that elevate the well-being of our furry friends.'
+	}
+];
+
+const secondViewDescription = [
+	{
+		id: 1,
+		class: 'secondary_section-one',
+		content: 'At DoGo, we are dedicated to the noble cause of preventing dog abandonment and ensuring every canine companion finds a loving home.'
+	},
+	{
+		id: 2,
+		class: 'secondary_section-two',
+		content:'Our mission is to create a seamless platform where compassionate individuals like you can make a difference in the lives of abandoned dogs.'
+	},
+	{
+		id: 3,
+		class: 'secondary_section-three',
+		content: 'Signal a Dog: With our user-friendly interface, you can easily signal a dog in need. Whether you come across a stray on the streets or learn about a neglected pup, simply provide the details, and our dedicated team will swing into action.'
+	},
+	{
+		id: 4,
+		class: 'secondary_section-four',
+		content: 'Rescue and Care: Our expert team of passionate dog lovers is committed to promptly responding to each distress signal. Equipped with the necessary skills and resources, they will rush to the location, ensuring the dog\'s safety.',
+		extra: 'We believe in providing immediate medical attention, nurturing care, and rehabilitation, allowing every dog to regain their health and happiness.'
+	},
+	{
+		id: 5,
+		class: 'secondary_section-five',
+		content: 'War Shelter: We understand that the journey to finding a forever home takes time. During this transition period, our well-equipped war shelters become a temporary haven for rescued dogs.',
+		extra: 'These shelters offer a safe and nurturing environment, where our furry friends receive love, companionship, and continuous medical support.'
+	},
+]
+ 
+export {firstViewDescription as dataHome, secondViewDescription as dataHome2};
